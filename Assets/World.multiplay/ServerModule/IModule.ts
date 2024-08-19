31bdda090bd4cd0f32e0fcc6ab4930ae031935a7
pjhppo <pjhppo@naver.com>
2024-08-20 00:10:27 +0900

    Rollback IModule

--- Assets/World.multiplay/ServerModule/IModule.ts
+++ Assets/World.multiplay/ServerModule/IModule.ts
@@ -1,13 +1,12 @@
 import { SandboxPlayer } from "ZEPETO.Multiplay";
+import Server from "..";
 
 export abstract class IModule {
-    private readonly _server: any;
-    protected get server(): any { return this._server; }
-
-    constructor(server: any) {
+    private readonly _server: Server;
+    protected get server(): Server { return this._server; }
+    constructor(server: Server) {
         this._server = server;
     }
-
     abstract OnCreate(): Promise<void>;
     abstract OnJoin(client: SandboxPlayer): Promise<void>;
     abstract OnLeave(client: SandboxPlayer): Promise<void>;
