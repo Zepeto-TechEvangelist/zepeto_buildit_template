efd8836116b9ba9dd503d98f9809f81fb6fdf1aa
Mario Josifovski <mario.dev@naverz-corp.com>
2024-12-23 16:04:59 +0900

    Update thumbnail script

--- Assets/Template_Resources/Interface/Scripts/PrefabThumbnailSaver.cs
+++ Assets/Template_Resources/Interface/Scripts/PrefabThumbnailSaver.cs
@@ -99,49 +99,98 @@ public class PrefabThumbnailSaver : EditorWindow
         }
         
         
+        // 렌더 텍스처 설정
+        RenderTexture rt = new RenderTexture(256, 256, 24);
+        mainCamera.targetTexture = rt;
+        mainCamera.clearFlags = CameraClearFlags.SolidColor;
+        mainCamera.backgroundColor = Color.clear;
+        
+        
+        
         // Itterate all prefabs in the folder
         var prefabs = GetPrefabs(sourceFolder);
 
+
+        
         foreach (var prefab in prefabs)
         {
             // Instantiate
-            var instance = Instantiate(prefab);
+            Texture2D preview = null; 
+            while (preview == null)
+                preview = AssetPreview.GetAssetPreview(prefab);// GetMiniThumbnail(Object obj);
+    
             
-            // Capture
-            // 렌더 텍스처 설정
-            RenderTexture rt = new RenderTexture(256, 256, 24);
-            mainCamera.targetTexture = rt;
-            mainCamera.clearFlags = CameraClearFlags.SolidColor;
-            mainCamera.backgroundColor = Color.clear;
-
-            // 카메라 렌더링
-            mainCamera.Render();
-
-            // 텍스처로부터 픽셀 읽기
-            RenderTexture.active = rt;
-            Texture2D screenShot = new Texture2D(256, 256, TextureFormat.RGBA32, false);
-            screenShot.ReadPixels(new Rect(0, 0, 256, 256), 0, 0);
-            screenShot.Apply();
-
-            // 리소스 정리
-            mainCamera.targetTexture = null;
-            RenderTexture.active = null;
-            DestroyImmediate(rt);
-
-            // PNG로 저장
-            byte[] bytes = screenShot.EncodeToPNG();
+            
+            
+            //
+            // GameObject instance = (GameObject)(GameObject.Instantiate(prefab, Vector3.zero, Quaternion.identity));
+            //
+            // // Capture
+            // // float perspectiveCompensation = 0.95f;
+            // //
+            // Bounds bounds = getBounds(instance);
+            // instance.transform.position = bounds.center;// += new Vector3(0, -bounds.max.y / 2.0f, 0); // center Y
+            // // Vector2 screenSize = new Vector2(Screen.width, Screen.height);
+            // // //Get the position on screen.
+            // // Vector2 screenPosition = mainCamera.WorldToScreenPoint(bounds.center);
+            // // //Get the position on screen from the position + the bounds of the object.
+            // // Vector2 sizePosition = mainCamera.WorldToScreenPoint(bounds.center + bounds.size);
+            // // //By subtracting the screen position from the size position, we get the size of the object on screen.
+            // // Vector2 objectSize = sizePosition - screenPosition;
+            // // //Calculate how many times the object can be scaled up.
+            // // Vector2 scaleFactor = screenSize / objectSize;
+            // // //The maximum scale is the one form the longest side, with the lowest scale factor.
+            // // float maximumScale = Mathf.Min(scaleFactor.x, scaleFactor.y);
+            //
+            // // if (mainCamera.orthographic)
+            // // {
+            // //     //Scale the orthographic size.
+            // //     mainCamera.orthographicSize = mainCamera.orthographicSize / maximumScale;
+            // // }
+            // // else
+            // // {
+            // //     //Set the scale of the object.
+            // //     instance.transform.localScale = instance.transform.localScale * maximumScale * perspectiveCompensation;
+            // // }
+            //
+            //
+            // //mainCamera.transform.LookAt(instance.transform);
+            // // Render It
+            // mainCamera.Render();
+            //
+            // // 텍스처로부터 픽셀 읽기
+            // RenderTexture.active = rt;
+            // Texture2D screenShot = new Texture2D(256, 256, TextureFormat.RGBA32, false);
+            // screenShot.ReadPixels(new Rect(0, 0, 256, 256), 0, 0);
+            // screenShot.Apply();
+            //
+            //
+            //
+            // // PNG로 저장
+            // byte[] bytes = screenShot.EncodeToPNG();
+            // string fileName = prefab.name + ".png";
+            // string filePath = Path.Combine(destinationFolder, fileName);
+            // File.WriteAllBytes(filePath, bytes);
+            //
+            // AssetDatabase.Refresh();
+            // Debug.Log("Screenshot saved to: " + filePath);
+            //
+            // // Clean Scene
+            // DestroyImmediate(instance);
+            
             string fileName = prefab.name + ".png";
             string filePath = Path.Combine(destinationFolder, fileName);
-            File.WriteAllBytes(filePath, bytes);
-
+            
+            File.WriteAllBytes(filePath, preview.EncodeToPNG());
+            
             AssetDatabase.Refresh();
             Debug.Log("Screenshot saved to: " + filePath);
-            
-            // Clean Scene
-            DestroyImmediate(instance);
         }
         
-        
+        // 리소스 정리
+        mainCamera.targetTexture = null;
+        RenderTexture.active = null;
+        DestroyImmediate(rt);
         
         
     }
@@ -171,6 +220,31 @@ public class PrefabThumbnailSaver : EditorWindow
         return assets;
     }
     
+    Bounds getBounds(GameObject objeto){
+        Bounds bounds;
+        Renderer childRender;
+        bounds = getRenderBounds(objeto);
+        if(bounds.extents.x == 0){
+            bounds = new Bounds(objeto.transform.position,Vector3.zero);
+            foreach (Transform child in objeto.transform) {
+                childRender = child.GetComponent<Renderer>();
+                if (childRender) {
+                    bounds.Encapsulate(childRender.bounds);
+                }else{
+                    bounds.Encapsulate(getBounds(child.gameObject));
+                }
+            }
+        }
+        return bounds;
+    }
+    Bounds getRenderBounds(GameObject objeto){
+        Bounds bounds = new  Bounds(Vector3.zero,Vector3.zero);
+        Renderer render = objeto.GetComponent<Renderer>();
+        if(render!=null){
+            return render.bounds;
+        }
+        return bounds;
+    }
     
     
 }
