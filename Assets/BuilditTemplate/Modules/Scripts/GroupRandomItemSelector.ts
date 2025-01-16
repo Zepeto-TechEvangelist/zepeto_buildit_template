da48f50647ed88ef80b43154c556b17467a83ecb
Mario Josifovski <mario.dev@naverz-corp.com>
2025-01-16 13:01:00 +0900

    Update reward chest object
    - no event chests are now hidden
    - added grouping
    - added randomizied selection per group

---
+++ Assets/BuilditTemplate/Modules/Scripts/GroupRandomItemSelector.ts
@@ -0,0 +1,22 @@
+import { GameObject } from 'UnityEngine'
+import { ZepetoScriptBehaviour } from 'ZEPETO.Script'
+import ObjectGroup from './ObjectGroup';
+
+export default class GroupRandomItemSelector extends ZepetoScriptBehaviour {
+    private _group: ObjectGroup;
+    private static _selectedIndex :int = -1;
+    
+    Awake() {
+        this._group = this.GetComponent<ObjectGroup>();
+    }
+    
+    Start() {
+        if (GroupRandomItemSelector._selectedIndex == -1)
+            GroupRandomItemSelector._selectedIndex = Math.round((Math.random() * this._group.members.length));
+        
+        console.log(GroupRandomItemSelector._selectedIndex)
+        console.log(this._group.members.length)
+        
+        this.gameObject.SetActive(this._group.members[GroupRandomItemSelector._selectedIndex] == this._group);
+    }
+}
---
+++ Assets/BuilditTemplate/Modules/Scripts/ObjectGroup.ts
@@ -0,0 +1,22 @@
+import { GameObject } from 'UnityEngine'
+import { ZepetoScriptBehaviour } from 'ZEPETO.Script'
+
+export default class ObjectGroup extends ZepetoScriptBehaviour {
+    
+    @Tooltip("The id of the group. Not editable after game start")
+    public groupId: string;
+    
+    @HideInInspector() public static groups: Map<string, ObjectGroup[]> = new Map<string, ObjectGroup[]>();
+    public get members(): [ObjectGroup] { 
+        return ObjectGroup.groups[this.groupId];
+    };
+    
+    Awake() 
+    {
+        if (ObjectGroup.groups[this.groupId] === undefined)
+            ObjectGroup.groups[this.groupId] = [];
+        
+        ObjectGroup.groups[this.groupId].push(this);
+    }
+    
+}
